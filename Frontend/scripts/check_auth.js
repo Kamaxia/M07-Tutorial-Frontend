2a0736b1cba3594d7fb024cf4294bf7f0aebaa34
Kami <150629561+Kamaxia@users.noreply.github.com>
2025-03-03 23:35:26 -0500

    Delete Frontend directory

--- Frontend/scripts/check_auth.js
+++
@@ -1,7 +0,0 @@
-function checkAuth(){
-    if(!localStorage.getItem("isLoggedIn")){
-        window.location.href = "/index.html"
-    }
-}
-
-checkAuth()
--- Frontend/scripts/logout.js
+++
@@ -1,4 +0,0 @@
-localStorage.removeItem("isLoggedIn")
-localStorage.removeItem("token")
-localStorage.removeItem("username")
-window.location.href = "/index.html"
